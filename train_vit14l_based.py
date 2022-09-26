a93c624d5fff8d3463f710e5c37b4dbadeac2cd7
yuan <ayrw@163.com>
2022-09-26 23:25:31 +0800

    M

---
+++ train_vit14l_based.py
@@ -0,0 +1,137 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as nnf
+from torch.utils.data import Dataset, DataLoader
+from enum import Enum
+from tqdm import tqdm
+import os
+import pickle
+import sys
+import argparse
+import json
+from typing import Tuple, Optional, Union
+from model import ClipCaptionModel,ClipCaptionPrefix,MappingType
+from dataset import ClipImageCaptionDataset
+from transformers import AdamW, get_linear_schedule_with_warmup
+
+
+def save_config(args: argparse.Namespace):
+    config = {}
+    for key, item in args._get_kwargs():
+        config[key] = item
+    out_path = os.path.join(args.out_dir, f"{args.prefix}.json")
+    with open(out_path, 'w') as outfile:
+        json.dump(config, outfile)
+
+
+def load_model(config_path: str, epoch_or_latest: Union[str, int] = '_latest'):
+    with open(config_path) as f:
+        config = json.load(f)
+    parser = argparse.ArgumentParser()
+    parser.set_defaults(**config)
+    args = parser.parse_args()
+    if type(epoch_or_latest) is int:
+        epoch_or_latest = f"-{epoch_or_latest:03d}"
+    model_path = os.path.join(args.out_dir, f"{args.prefix}{epoch_or_latest}.pt")
+    if args.only_prefix:
+        model = ClipCaptionPrefix(args.prefix_length)
+    else:
+        model = ClipCaptionModel(args.prefix_length)
+    if os.path.isfile(model_path):
+        print(f"loading model from {model_path}")
+        model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
+    else:
+        print(f"{model_path} is not exist")
+    return model, parser
+
+
+def recover_checkpoint(checkPointPath,clipCaptionModel):
+    if os.path.isfile(checkPointPath):
+        print(f"loading model from {checkPointPath}")
+        clipCaptionModel.load_state_dict(torch.load(checkPointPath, map_location=torch.device('cpu')))
+    else:
+        print(f"{checkPointPath} is not exist")
+
+
+def train(dataset: ClipImageCaptionDataset, model: ClipCaptionModel, args,
+          lr: float = 2e-6, warmup_steps: int = 5000, output_dir: str = ".", output_prefix: str = ""):
+
+    device = torch.device('cuda:0')
+    batch_size = args.bs
+    epochs = args.epochs
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    model = model.to(device)
+    model.train()
+    optimizer = AdamW(model.parameters(), lr=lr)
+    train_dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, drop_last=True)
+    print('Fianl train ds len:',len(dataset))
+    scheduler = get_linear_schedule_with_warmup(
+        optimizer, num_warmup_steps=warmup_steps, num_training_steps=epochs * len(train_dataloader)
+    )
+    # save_config(args)
+    for epoch in range(epochs):
+        print(f">>> Training epoch {epoch}")
+        sys.stdout.flush()
+        progress = tqdm(total=len(train_dataloader), desc=output_prefix)
+        for idx, (tokens, mask, prefix) in enumerate(train_dataloader):
+            model.zero_grad()
+            tokens, mask, prefix = tokens.to(device), mask.to(device), prefix.to(device, dtype=torch.float32)
+            outputs = model(tokens, prefix, mask)
+            logits = outputs.logits[:, dataset.prefix_length - 1: -1]
+            loss = nnf.cross_entropy(logits.reshape(-1, logits.shape[-1]), tokens.flatten(), ignore_index=0)
+            loss.backward()
+            optimizer.step()
+            scheduler.step()
+            optimizer.zero_grad()
+            progress.set_postfix({"loss": loss.item()})
+            progress.update()
+            if (idx + 1) % 10000 == 0:
+                torch.save(
+                    model.state_dict(),
+                    os.path.join(output_dir, f"{output_prefix}_latest.pt"),
+                )
+        progress.close()
+        if epoch % args.save_every == 0 or epoch == epochs - 1:
+            torch.save(
+                model.state_dict(),
+                os.path.join(output_dir, f"{output_prefix}-{epoch:03d}.pt"),
+            )
+    return model
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data', default="F:/COCO_DS/full_coco_ViT-L_14@336px_train.pkl")
+    parser.add_argument('--out_dir', default='F:/COCO_DS/checkpoints')
+    parser.add_argument('--prefix', default='coco_vit14l_336px_prefix', help='prefix for saved filenames')
+    parser.add_argument('--epochs', type=int, default=1000)
+    parser.add_argument('--save_every', type=int, default=100)
+    parser.add_argument('--prefix_length', type=int, default=10)
+    parser.add_argument('--prefix_length_clip', type=int, default=10)
+    parser.add_argument('--bs', type=int, default=25)
+    parser.add_argument('--only_prefix', dest='only_prefix', action='store_true')
+    parser.add_argument('--mapping_type', type=str, default='mlp', help='mlp/transformer')
+    parser.add_argument('--num_layers', type=int, default=8)
+    parser.add_argument('--is_rn', dest='is_rn', action='store_true')
+    parser.add_argument('--normalize_prefix', dest='normalize_prefix', action='store_true')
+    args = parser.parse_args()
+    prefix_length = args.prefix_length
+    dataset = ClipImageCaptionDataset(args.data, prefix_length, normalize_prefix=args.normalize_prefix)
+    prefix_dim = 768
+    args.mapping_type = {'mlp': MappingType.MLP, 'transformer': MappingType.Transformer}[args.mapping_type]
+    if args.only_prefix:
+        model = ClipCaptionPrefix(prefix_length, clip_length=args.prefix_length_clip, prefix_size=prefix_dim,
+                                  num_layers=args.num_layers, mapping_type=args.mapping_type)
+        print("Train only prefix")
+    else:
+        model = ClipCaptionModel(prefix_length, clip_length=args.prefix_length_clip, prefix_size=prefix_dim,
+                                  num_layers=args.num_layers, mapping_type=args.mapping_type)
+        print("Train both prefix and GPT")
+        sys.stdout.flush()
+    # recover_checkpoint('conceptual_weights.pt',model)#(r"F:\COCO_DS\checkpoints\coco_prefix-008-final.pt",model)
+    train(dataset, model, args, output_dir=args.out_dir, output_prefix=args.prefix)
+
+
+if __name__ == '__main__':
+    main()
